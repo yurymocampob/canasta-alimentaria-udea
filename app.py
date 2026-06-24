bfbe98167d50784c6803195ca80c5f6d0d66034f
yurymocampob <yury.ocampo@udea.edu.co>
2026-06-24 15:26:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -65,8 +65,8 @@ except Exception as e:
 
 st.title("🌽Precio de alimentos por territorialidades alimentarias")
 st.write("Formulario para la recolección de precios de alimentos de las 13 territorialidades colombianas discriminados por tienda, plaza de mercado y supermercado. Indicaciones:")
-st.write("1. Ingrese a la aplicación con su email institucional")
-st.write("2. Registre los precios de los alimentos en la misma unidad de medida para los tres expendios.")
+st.write("1. Ingrese a la aplicación con su email institucional.")
+st.write("2. Registre los precios de los alimentos para los tres expendios.")
 
 st.subheader("1. Validación Institucional y Ubicación")
 correo_estudiante = st.text_input("Correo Institucional (@udea.edu.co) *", key="corr_est").strip().lower()
